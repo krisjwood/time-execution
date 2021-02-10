e97087c44f8fdc4904ceb148bd1db55484214ef7
Kris <64105912+krjw-00@users.noreply.github.com>
2021-02-11 11:09:50 +1300

    Script
    Python script

---
+++ x_script.py
@@ -0,0 +1,35 @@
+'''Function that allows for the coder to insert a start and end point for the progreamme to track the time it took to execute.'''
+
+''' Instructions:
+
+# Put at start point:
+    start = xstart()
+
+# Put at end point:
+    xend(start) 
+'''
+
+# Start
+def xstart():
+    import time
+    start = time.time()
+    return start
+
+# End
+def xend(start):
+    import time
+    end = time.time()
+
+    # Summary
+    execution = end - start
+    rounded = round(execution, 4)
+    print("\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print(f"S: {time.ctime(start)}    E: {time.ctime(end)}   ({rounded} seconds)")
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+# Operation
+start = xstart()
+range_num = 10000
+[print(i) for i in range(range_num)]
+xend(start)
